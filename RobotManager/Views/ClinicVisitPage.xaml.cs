42e0fdd51ec386eb0bd297e13c4e9cb7a84feeff
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-09-17 00:21:56 +0200

    added view to add clinic visits

--- RobotManager/Views/ClinicVisitPage.xaml.cs
+++ RobotManager/Views/ClinicVisitPage.xaml.cs
@@ -5,17 +5,18 @@ namespace RobotManager.Views;
 public partial class ClinicVisitPage : ContentPage
 {
 	Nao _nao;
-	ClinicVisit clinicVisit = new();
-	public ClinicVisitPage(Nao nao)
+	ClinicVisit _clinicVisit;
+	public ClinicVisitPage(Nao nao, ClinicVisit clinicVisit, bool creatorMode = false)
 	{
 		InitializeComponent();
 		BindingContext = nao;
 		_nao = nao;
-	}
+        _clinicVisit = clinicVisit;
+    }
 
     private void CreateVisit_Button_Clicked(object sender, EventArgs e)
     {
-		_nao.ClinicVisits.Add(clinicVisit);
+		_nao.ClinicVisits.Add(_clinicVisit);
 		_nao.Status = Status.Clinic;
     }
 }
