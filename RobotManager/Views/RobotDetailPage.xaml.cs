a298b66526742ff31b80334c1cae8c7d3166acd3
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-09-17 00:18:34 +0200

    added view to view robot details

--- RobotManager/Views/RobotDetailPage.xaml.cs
+++ RobotManager/Views/RobotDetailPage.xaml.cs
@@ -4,10 +4,44 @@ namespace RobotManager.Views;
 
 public partial class RobotDetailPage : ContentPage
 {
-	public RobotDetailPage(Nao nao)
-	{
-		InitializeComponent();
+    private Nao _nao;
+    public RobotDetailPage(Nao nao)
+    {
+        InitializeComponent();
 
-		BindingContext = nao;
-	}
+        BindingContext = nao;
+        _nao = nao;
+
+        bool underWarranty = nao.Warranty >= DateTime.Now;
+        warrantyLabel.Text = underWarranty ? $"Currently under warranty ({nao.Warranty.ToString("dd.MM.yyyy")})" : "Not under warranty";
+    }
+
+    private void AddButton_Clicked(object sender, EventArgs e)
+    {
+        AddInterface.IsVisible = !AddInterface.IsVisible;
+        //AddButton.Text = AddButton.Text == "+" ? "x" : "+";
+        double currRot = AddButton.Rotation;
+        AddButton.RotateTo(currRot == 0 ? 45 : 0);
+    }
+
+    private void AddNavigationButton_Clicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        ContentPage page = new ContentPage();
+
+        if (button == AddIssueButton)
+        {
+            page = new IssuePage(_nao, new(), true);
+        }
+        else if (button == AddNoteButton)
+        {
+            page = new NotePage(_nao, new(), true);
+        }
+        else if (button == AddClinicVisitButton)
+        {
+            page = new ClinicVisitPage(_nao, new(), true);
+        }
+
+        Navigation.PushAsync(page);
+    }
 }
