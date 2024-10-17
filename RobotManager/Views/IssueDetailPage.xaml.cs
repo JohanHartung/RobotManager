4d618354654791308e09eba295ba67456001f4e3
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-10-17 17:08:35 +0200

    added page content

--- RobotManager/Views/IssueDetailPage.xaml.cs
+++ RobotManager/Views/IssueDetailPage.xaml.cs
@@ -10,4 +10,27 @@ public partial class IssueDetailPage : ContentPage
 
 		BindingContext = issue;
 	}
+
+    private void OptionsButton_Clicked(object sender, EventArgs e)
+    {
+        OptionsInterface.IsVisible = !OptionsInterface.IsVisible;
+        //AddButton.Text = AddButton.Text == "+" ? "x" : "+";
+        double currRot = OptionsButton.Rotation;
+        OptionsButton.RotateTo(currRot == 0 ? 45 : 0);
+    }
+
+    private void ReplicateButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void SolveButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void RemoveButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
--- RobotManager/Views/RobotDetailPage.xaml.cs
+++ RobotManager/Views/RobotDetailPage.xaml.cs
@@ -5,15 +5,29 @@ namespace RobotManager.Views;
 public partial class RobotDetailPage : ContentPage
 {
     private Nao _nao;
-    public RobotDetailPage(Nao nao)
+    private Grid? currentIssueButtons;
+
+    List<Issue> _issues = new();
+    List<Note> _notes = new();
+    List<ClinicVisit> _clinicVisits = new();
+
+    public RobotDetailPage(Nao nao, List<Issue> issues, List<Note> notes, List<ClinicVisit> clinicVisits)
     {
         InitializeComponent();
 
-        BindingContext = nao;
         _nao = nao;
+        _issues = issues;
+        _notes = notes;
+        _clinicVisits = clinicVisits;
 
         bool underWarranty = nao.Warranty >= DateTime.Now;
         warrantyLabel.Text = underWarranty ? $"Currently under warranty ({nao.Warranty.ToString("dd.MM.yyyy")})" : "Not under warranty";
+
+
+        BindingContext = _nao;
+        IssueCV.ItemsSource = _issues;
+        NoteCV.ItemsSource = _notes;
+        ClinicCV.ItemsSource = _clinicVisits;
     }
 
     private void AddButton_Clicked(object sender, EventArgs e)
@@ -31,17 +45,60 @@ public partial class RobotDetailPage : ContentPage
 
         if (button == AddIssueButton)
         {
-            page = new IssuePage(_nao, new(), true);
+            page = new AddIssuePage(_nao);
         }
         else if (button == AddNoteButton)
         {
-            page = new NotePage(_nao, new(), true);
+            page = new AddNotePage(_nao);
         }
         else if (button == AddClinicVisitButton)
         {
-            page = new ClinicVisitPage(_nao, new(), true);
+            page = new AddClinicVisitPage(_nao, _issues);
         }
 
         Navigation.PushAsync(page);
+    }
+
+    private void Frame_Tapped(object sender, TappedEventArgs e)
+    {
+        var frame = sender as Frame;
+        if (frame == null) { return; }
+        var layout = frame.Content as VerticalStackLayout;
+        var issueButtons = layout.FindByName<Grid>("DetailButtons");
+
+        if (!issueButtons.IsVisible)
+        {
+            if (currentIssueButtons != null)
+            {
+                currentIssueButtons.IsVisible = false;
+            }
+            issueButtons.IsVisible = true;
+            currentIssueButtons = issueButtons;
+        }
+        else
+        {
+            issueButtons.IsVisible = false;
+            currentIssueButtons = null;
+        }
+    }
+
+    private void ViewEditIssueButton_Clicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button == null) { return; }
+        var issue = button.BindingContext as Issue;
+        if (issue == null) { return; }
+        Navigation.PushAsync(new IssueDetailPage(issue));
+    }
+
+    private async void SolvedIssueButton_Clicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button == null) { return; }
+        var issue = button.BindingContext as Issue;
+        if (issue == null) { return; }
+
+        string solvedReport = await DisplayPromptAsync("Solved Report", "If issue could not be replicated, leave empty");
+        //TODO: Update issue with solved report and date
     }
 }
