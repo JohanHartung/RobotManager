47da69f61ffe9f164a76749816422bd3ac7eec76
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-10-17 17:06:07 +0200

    added selection for issue list

--- RobotManager/Views/AddClinicVisitPage.xaml.cs
+++ RobotManager/Views/AddClinicVisitPage.xaml.cs
@@ -5,14 +5,17 @@ namespace RobotManager.Views;
 public partial class AddClinicVisitPage : ContentPage
 {
 	Nao _nao;
-	ClinicVisit _clinicVisit;
-	List<int> issues = new();
-	public AddClinicVisitPage(Nao nao, ClinicVisit clinicVisit, bool creatorMode = false)
+	ClinicVisit clinicVisit;
+
+	List<Issue> selectedIssues = new();
+
+	public AddClinicVisitPage(Nao nao, List<Issue> issues)
 	{
 		InitializeComponent();
 		BindingContext = nao;
 		_nao = nao;
-        _clinicVisit = clinicVisit;
+		clinicVisit = new();
+		IssueCV.ItemsSource = issues;
     }
 
     private void CreateVisit_Button_Clicked(object sender, EventArgs e)
@@ -22,4 +25,23 @@ public partial class AddClinicVisitPage : ContentPage
 		_nao.Status = Status.Clinic;
     }
 
+	private void IssueFrame_Tapped(object sender, TappedEventArgs e)
+	{
+		var frame = sender as Frame;
+		var issue = frame.BindingContext as Issue;
+		if (issue == null) { return; }
+		Color normal = Color.FromArgb("#303030");
+		Color selected = Color.FromArgb("#455a64");
+
+		if (selectedIssues.Contains(issue))
+		{
+			selectedIssues.Remove(issue);
+			frame.BackgroundColor = normal;
+		}
+		else
+		{
+			selectedIssues.Add(issue);
+			frame.BackgroundColor = selected;
+		}
+	}
 }
