3949a537af15de9d1c025fecc36fa99de95460d0
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-11-07 12:02:26 +0100

    added edit mode

--- RobotManager/Views/AddIssuePage.xaml.cs
+++ RobotManager/Views/AddIssuePage.xaml.cs
@@ -5,14 +5,34 @@ namespace RobotManager.Views;
 
 public partial class AddIssuePage : ContentPage
 {
-	Nao _nao;
-    Issue issue = new();
-	public AddIssuePage(Nao nao)
-	{
-		InitializeComponent();
+    Nao _nao;
+    Issue _issue = new();
+    bool _editMode;
+    public AddIssuePage(Nao nao, Issue? issue = null, bool editMode = false)
+    {
+        InitializeComponent();
         ResetDateTime();
         BindingContext = nao;
-		_nao = nao;
+        _nao = nao;
+        _issue = issue ?? new Issue();
+        _editMode = editMode;
+        if (_editMode)
+        {
+            Title = $"Edit Issue #{_issue.Id.ToString().PadLeft(4, '0')} | NAO{_nao.Name}";
+            CreateIssueButton.Text = "Save Changes";
+            TitleEntry.Text = _issue.Title;
+            IssueDatePicker.Date = _issue.Date;
+            IssueTimePicker.Time = _issue.Date.TimeOfDay;
+            IssueDescription.Text = _issue.Description;
+            ReplicatedCheckBox.IsChecked = _issue.Replicated;
+            ReplicatedDatePicker.Date = _issue.ReplicatedDate;
+            ReplicatedTimePicker.Time = _issue.ReplicatedDate.TimeOfDay;
+            SolvedCheckBox.IsChecked = _issue.Solved;
+            SolvedDatePicker.Date = _issue.SolvedDate;
+            SolvedTimePicker.Time = _issue.SolvedDate.TimeOfDay;
+            SolvedReport.Text = _issue.SolvedReport;
+        }
+
     }
 
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
@@ -24,34 +44,21 @@ public partial class AddIssuePage : ContentPage
     {
         if (FormIsValid())
         {
-            Issue issue = new()
-            {
-                Title = TitleEntry.Text,
-                Date = IssueDatePicker.Date.Add(IssueTimePicker.Time),
-                Description = IssueDescription.Text,
-                Replicated = ReplicatedCheckBox.IsChecked,
-                ReplicatedDate = ReplicatedDatePicker.Date.Add(ReplicatedTimePicker.Time),
-                Solved = SolvedCheckBox.IsChecked,
-                SolvedDate = SolvedDatePicker.Date.Add(SolvedTimePicker.Time),
-                SolvedReport = SolvedReport.Text,
-                Nao = _nao.Id
-            };
-            try
-            {
-                await PostIssueAsync(issue);
-            }
-            catch (Exception)
+            _issue.Title = TitleEntry.Text;
+            _issue.Date = IssueDatePicker.Date.Add(IssueTimePicker.Time);
+            _issue.Description = IssueDescription.Text;
+            _issue.Replicated = ReplicatedCheckBox.IsChecked;
+            _issue.ReplicatedDate = ReplicatedDatePicker.Date.Add(ReplicatedTimePicker.Time);
+            _issue.Solved = SolvedCheckBox.IsChecked;
+            _issue.SolvedDate = SolvedDatePicker.Date.Add(SolvedTimePicker.Time);
+            _issue.SolvedReport = SolvedReport.Text;
+            _issue.Nao = _nao.Id;
+
+            if (!await _issue.Post())
             {
                 await DisplayAlert("Error", "Could not connect to the server ", "OK");
             }
         }
-    }
-
-    private void MoreButton_Clicked(object sender, EventArgs e)
-    {
-        IssueDescription.HeightRequest = 100;
-        MoreButton.IsVisible = false;
-        MoreOptions.IsVisible = true;
     }
 
     private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
