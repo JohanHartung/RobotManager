407c72565ec7805aa7fd1f6a8dc2fdd736988b93
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-11-07 11:59:25 +0100

    added issue SolveButton functionality

--- RobotManager/Views/IssueDetailPage.xaml.cs
+++ RobotManager/Views/IssueDetailPage.xaml.cs
@@ -33,7 +33,24 @@ public partial class IssueDetailPage : ContentPage
         
     }
 
-    private void SolveButton_Clicked(object sender, EventArgs e)
+    private async void SolveButton_Clicked(object sender, EventArgs e)
+    {
+        string solvedReport = await DisplayPromptAsync("Solved Report", "If issue could not be replicated, leave empty");
+        if (solvedReport == null)
+        {
+            // User pressed Cancel
+            // use string.IsNullOrWhiteSpace(solvedReport) to check if user pressed OK with empty input
+            return;
+        }
+        _issue.Solved = true;
+        _issue.SolvedDate = DateTime.Now;
+        _issue.SolvedReport = solvedReport;
+        if (!await _issue.Post())
+        {
+            await DisplayAlert("Error", "Could not connect to the server", "OK");
+        }
+    }
+
     private async void RemoveButton_Clicked(object sender, EventArgs e)
     {
         bool answer = await DisplayAlert("Delete", "Are you sure you want to delete this note?", "Yes", "No");
