373d12ee68bd5d73c8c6daacc0cda9a7e11db06f
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-12-14 18:28:17 +0100

    added user

---
+++ RobotManager/Classes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotManager.Classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? DeviceId { get; set; }
+        public string? Token { get; set; }
+
+
+    }
+}
