0ae424924980b2469882326f58fbaad04d342b4e
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-12-20 18:06:42 +0100

    added settings

--- RobotManager/Views/SettingsPage.xaml.cs
+++ RobotManager/Views/SettingsPage.xaml.cs
@@ -5,5 +5,17 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}
+        uriText.Text = Preferences.Get("uri", "https://example.com/api/RobotManager/");
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        uriText.Text = $"https://{domainEntry.Text}/api/RobotManager/";
+        Preferences.Set("uri", domainEntry.Text);
+    }
+
+    private void domainEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        uriText.Text = $"https://{domainEntry.Text}/api/RobotManager/";
+    }
 }
