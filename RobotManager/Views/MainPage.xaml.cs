dcf038fa0feeee25b233c21c7351f1665dc0f7af
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2025-03-06 15:36:22 +0100

    small changes for debugging

--- RobotManager/Views/MainPage.xaml.cs
+++ RobotManager/Views/MainPage.xaml.cs
@@ -1,13 +1,34 @@
 ﻿namespace RobotManager.Views;
+using static Classes.Helpers;
+using Classes;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+#if DEBUG
+        resetButton.IsVisible = true;
+        loginButton.IsVisible = true;
+#endif
     }
 
-    
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Preferences.Set("DeviceId", null);
+        Preferences.Set("Token", null);
+        Preferences.Set("UserId", -1);
+        Preferences.Set("UserName", null);
+    }
+
+    private async void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        User user = new();
+        string username = "Johan";
+        string password = Hash("1357");
+        var userdata = await user.CreateUser(username, password);
+        user.Initialize(userdata);
+    }
 }
 
 
