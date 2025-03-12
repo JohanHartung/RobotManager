55e5215905fde32262010f3849482b4e680f28ab
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2025-03-12 12:03:50 +0100

    added upcoming games

--- RobotManager/Views/MainPage.xaml.cs
+++ RobotManager/Views/MainPage.xaml.cs
@@ -1,11 +1,14 @@
 ﻿namespace RobotManager.Views;
 using static Classes.Helpers;
 using Classes;
+using System.Timers;
 
 public partial class MainPage : ContentPage
 {
 
+    private List<Game>? games = new();
     private List<ClinicVisit>? clinicVisits = new();
+    public System.Timers.Timer timer;
 
     public MainPage()
     {
@@ -15,8 +18,11 @@ public partial class MainPage : ContentPage
 
     private async Task InitializeAsync()
     {
+        games = await GetAllGames();
+        GamesCV.ItemsSource = games;
         clinicVisits = await GetAllClinicVisits();
         ClinicVisitCV.ItemsSource = clinicVisits;
+        StartCountdown();
     }
 
     
@@ -34,6 +40,43 @@ public partial class MainPage : ContentPage
     {
         await InitializeAsync();
         MainPageRV.IsRefreshing = false;
+    }
+
+    private void StartCountdown()
+    {
+        timer = new System.Timers.Timer(1000);
+        timer.Elapsed += TimerElapsed;
+        timer.Start();
+    }
+
+    private void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        foreach (var game in games)
+        {
+            var remaining = game.Date - DateTime.Now;
+
+            if (remaining.TotalSeconds <= 0)
+            {
+                game.CountdownText = "Started!";
+            }
+            else if (remaining.TotalMinutes <= 60)
+            {
+                game.CountdownText = $"{remaining.Minutes:D2}:{remaining.Seconds:D2}";
+            }
+            else
+            {
+                game.CountdownText = null;
+            }
+        }
+
+        // Refresh the UI on the main thread
+        Dispatcher.Dispatch(() => GamesCV.ItemsSource = null);
+        Dispatcher.Dispatch(() => GamesCV.ItemsSource = games);
+    }
+
+    private async void AddGameButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddGamePage());
     }
 }
 
