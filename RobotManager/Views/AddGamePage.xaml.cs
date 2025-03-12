9e330a069c66bcf19e0a3409e4e3a6ea589994e5
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2025-03-12 12:02:57 +0100

    added add game page

---
+++ RobotManager/Views/AddGamePage.xaml.cs
@@ -0,0 +1,75 @@
+using RobotManager.Classes;
+
+namespace RobotManager.Views;
+
+public partial class AddGamePage : ContentPage
+{
+    private Game game = new();
+	public AddGamePage()
+	{
+		InitializeComponent();
+        ResetDateTime();
+	}
+
+    private void HomeCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var checkBox = sender as CheckBox;
+        game.Home = checkBox!.IsChecked;
+    }
+
+    private async void CreateGameButton_Clicked(object sender, EventArgs e)
+    {
+        if (FormIsValid())
+        {
+            game.Against = OpponentEntry.Text;
+            game.Date = IssueDatePicker.Date.Add(IssueTimePicker.Time);
+            if (HomeCheckBox.IsChecked)
+            {
+                game.Home = HomeCheckBox.IsChecked;
+            }
+
+            if (!await game.Post())
+            {
+                await DisplayAlert("Error", "Could not connect to the server ", "OK");
+            }
+            else
+            {
+                await Navigation.PopAsync();
+            }
+        }
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        ResetDateTime();
+    }
+
+    private void ResetDateTime()
+    {
+        IssueDatePicker.Date = DateTime.Now;
+        IssueTimePicker.Time = DateTime.Now.TimeOfDay;
+    }
+
+    private bool FormIsValid()
+    {
+        if (string.IsNullOrEmpty(OpponentEntry.Text))
+        {
+            DisplayAlert("Error", "Opponent is required", "OK");
+            return false;
+        }
+        return true;
+    }
+
+    private void picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = sender as Picker;
+        game.Field = picker!.SelectedIndex switch
+        {
+            0 => Field.A,
+            1 => Field.B,
+            2 => Field.C,
+            3 => Field.D,
+            _ => Field.A
+        };
+    }
+}
